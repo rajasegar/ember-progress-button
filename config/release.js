6be75e20f306735d178e70755af7b0212206024d
Rajasegar Chandran <rajasegar.c@gmail.com>
2017-06-27 10:56:22 +0530

    Adding demoUrl in package json and release config cleanup

--- config/release.js
+++ config/release.js
@@ -4,26 +4,12 @@ var execSync = require('child_process').execSync;
 
 // For details on each option run `ember help release`
 module.exports = {
-  // local: true,
-  // remote: 'some_remote',
-  // annotation: "Release %@",
-  // message: "Bumped version to %@",
-  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
   publish: true,
   afterPublish: function(project, versions) {
     // Publish dummy app with docs to gh-pages
     runCommand('ember github-pages:commit --message "Released ' + versions.next + '"');
     runCommand('git push origin gh-pages:gh-pages');
   }
-  // strategy: 'date',
-  // format: 'YYYY-MM-DD',
-  // timezone: 'America/Los_Angeles',
-  //
-  // beforeCommit: function(project, versions) {
-  //   return new RSVP.Promise(function(resolve, reject) {
-  //     // Do custom things here...
-  //   });
-  // }
 };
 
 function runCommand(command) {
