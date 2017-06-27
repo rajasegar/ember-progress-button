7c7988b6e2e2d80c5c20e18be9e346ef20ba8a75
Rajasegar Chandran <rajasegar.c@gmail.com>
2017-06-27 10:51:06 +0530

    Adding release and documentation tools

---
+++ config/release.js
@@ -0,0 +1,33 @@
+/* jshint node:true */
+// var RSVP = require('rsvp');
+var execSync = require('child_process').execSync;
+
+// For details on each option run `ember help release`
+module.exports = {
+  // local: true,
+  // remote: 'some_remote',
+  // annotation: "Release %@",
+  // message: "Bumped version to %@",
+  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
+  publish: true,
+  afterPublish: function(project, versions) {
+    // Publish dummy app with docs to gh-pages
+    runCommand('ember github-pages:commit --message "Released ' + versions.next + '"');
+    runCommand('git push origin gh-pages:gh-pages');
+  }
+  // strategy: 'date',
+  // format: 'YYYY-MM-DD',
+  // timezone: 'America/Los_Angeles',
+  //
+  // beforeCommit: function(project, versions) {
+  //   return new RSVP.Promise(function(resolve, reject) {
+  //     // Do custom things here...
+  //   });
+  // }
+};
+
+function runCommand(command) {
+  console.log('running: ' + command);
+  var output = execSync(command, { encoding: 'utf8' });
+  console.log(output);
+}
